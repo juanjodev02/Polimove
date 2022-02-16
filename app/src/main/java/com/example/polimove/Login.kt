985479e0df0a789150600cf24c13b4322ba9c9d1
Bryan <bryan.flores04@epn.edu.ec>
2022-02-16 07:52:31 -0500

    Corrección vistas

---
+++ app/src/main/java/com/example/polimove/Login.kt
@@ -0,0 +1,59 @@
+package com.example.polimove
+
+
+
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
+
+
+class Login : AppCompatActivity() {
+
+//    var fragmentTransaction = fragmentManager.beginTransaction();
+//    val fragmentStartUpFragment = StartUpFragment();
+//    val fragmentLoginFragment = LoginFragment();
+//    val mHandler = Handler()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+//        supportFragmentManager.beginTransaction().add(R.id.fragmentStartUp,fragmentStartUpFragment).commit()
+//        R.id.fragmentStartUp.
+
+    // Crea el nuevo fragmento y la transacción.
+    // Crea el nuevo fragmento y la transacción.
+
+
+    // Commit a la transacción
+
+    // Commit a la transacción
+//    transaction.commit()
+
+    }
+
+
+
+//    fun onClick(){
+//        print(5);
+//    }
+
+//    fun changeFragment(v: View?) {
+//        //mHandler.postDelayed(mToastRunnable, 5000);
+//        mToastRunnable.run()
+//
+//    }
+//
+//    private val mToastRunnable: Runnable = object : Runnable {
+//        override fun run() {
+//            val fm:  = fragmentManager
+//            fm.beginTransaction()
+//                .setCustomAnimations(android.R.animator.fade_in, android.R.animator.fade_out)
+//                .show(somefrag)
+//                .commit()
+//                mHandler.postDelayed(this, 5000)
+//        }
+//    }
+}
