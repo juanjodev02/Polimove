bd9b712ce0ac02ea8fa73863450bc96acf057942
Andrea Cachipuendo <andreavs05@hotmail.com>
2022-02-28 10:05:25 -0500

    Query name and email about profile.

--- app/src/main/java/com/example/polimove/ui/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/polimove/ui/profile/ProfileFragment.kt
@@ -1,11 +1,17 @@
 package com.example.polimove.ui.profile
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
+import android.view.KeyCharacterMap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentResultListener
 import androidx.lifecycle.ViewModelProvider
 import com.example.polimove.databinding.FragmentProfileBinding
 
@@ -13,35 +19,65 @@ class ProfileFragment : Fragment() {
 
     private var _binding: FragmentProfileBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    //INICILIZACIÓN DE VARIABLES
+    private var cedula:String? = ""
+    private lateinit var textViewName: TextView
+    private lateinit var textViewEmail: TextView
+    private lateinit var buttonSignOut: Button
+    private lateinit var buttonDelete: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileViewModel =
-            ViewModelProvider(this).get(ProfileViewModel::class.java)
+        /*val profileViewModel =
+            ViewModelProvider(this).get(ProfileViewModel::class.java)*/
 
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textViewNomCompletoEst: TextView = binding.textViewEstName
-        profileViewModel.fullNameStd.observe(viewLifecycleOwner) {
+        textViewName = binding.textViewEstName
+        /*profileViewModel.fullNameStd.observe(viewLifecycleOwner) {
             textViewNomCompletoEst.text = it
-        }
+        }*/
 
-        val textViewCorreoEst: TextView = binding.textViewEstEmail
-        profileViewModel.emailStd.observe(viewLifecycleOwner) {
+        textViewEmail = binding.textViewEstEmail
+        /*profileViewModel.emailStd.observe(viewLifecycleOwner) {
             textViewCorreoEst.text = it
+        }*/
+
+        buttonSignOut = binding.buttonContinuar
+        buttonDelete = binding.buttonEliminarDatos
+        /*profileViewModel.numberStd.observe(viewLifecycleOwner) {
+            textViewCelularEst.text = it
+        }*/
+
+        parentFragmentManager.setFragmentResultListener("key",this, FragmentResultListener(
+            Key, result->
+        cedula = result.getString("cedula")
+        Log.d("CI","La cédula: $cedula")
+        UserService.getData(cedula as String){ nameUser ->
+            textViewName.text = nameUser.name+" "+nameUser.lastName
+            textViewEmail.text = nameUser.email
+        }
+        ))
+
+        buttonSignOut.setOnClickListener {
+            UserService.signOff()
+        }
+        buttonDelete.setOnClickListener {
+            UserService.getUserId(cedula)
+            val activity: Activity? = activity
+            Toast.makeText(activity, "Datos eliminados exitosamente.", Toast.LENGTH_LONG)
         }
 
-        val textViewCelularEst: TextView = binding.textViewEstTelefono
-        profileViewModel.numberStd.observe(viewLifecycleOwner) {
-            textViewCelularEst.text = it
-        }
         return root
     }
 
