6e7c55f46b81de10f061a46e9b2cba8b618eace3
Andrea Cachipuendo <andreavs05@hotmail.com>
2022-02-28 12:28:25 -0500

    Changed ProfileDriverFragment.kt

--- app/src/main/java/com/example/polimove/ui/profile/ProfileDriverFragment.kt
+++ app/src/main/java/com/example/polimove/ui/profile/ProfileDriverFragment.kt
@@ -1,45 +1,69 @@
 package com.example.polimove.ui.profile
 
+import android.app.Activity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentResultListener
 import androidx.lifecycle.ViewModelProvider
+import com.example.polimove.Service.UserService
 import com.example.polimove.databinding.FragmentProfileBinding
 
 class ProfileDriverFragment : Fragment() {
     private var _binding: FragmentProfileBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    //INICIALIZACIÓN DE VARIABLES
+    private var cedulaDriver:String?=""
+    private lateinit var textViewNameDriver: TextView
+    private lateinit var textViewEmailDriver: TextView
+    private lateinit var buttonSignOutDriver: Button
+    private lateinit var buttonDeleteDriver: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val profileDriveViewModel =
-            ViewModelProvider(this).get(ProfileDriverViewModel::class.java)
+
         _binding = FragmentProfileBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textViewNomCompletoEst: TextView = binding.textViewEstName
-        profileDriveViewModel.fullNameDriver.observe(viewLifecycleOwner) {
-            textViewNomCompletoEst.text = it
+        textViewNameDriver= binding.textViewEstName
+        textViewEmailDriver= binding.textViewEstEmail
+        buttonSignOutDriver = binding.buttonContinuar
+        buttonDeleteDriver = binding.buttonEliminarDatos
+
+        parentFragmentManager.setFragmentResultListener("key",this, FragmentResultListener {
+                Key, result ->
+            cedulaDriver = result.getString("cedula")
+            Log.d("CI", "La cédula: $cedulaDriver")
+            UserService.getData(cedulaDriver as String) { nameUser ->
+                textViewNameDriver.text = nameUser.name + " " + nameUser.lastName
+                textViewEmailDriver.text = nameUser.email
+            }
+        })
+
+        buttonSignOutDriver.setOnClickListener {
+            UserService.signOff()
+        }
+        buttonDeleteDriver.setOnClickListener {
+            UserService.getUserId(cedulaDriver)
+            val activity: Activity? = activity
+            Toast.makeText(activity, "Datos eliminados exitosamente.", Toast.LENGTH_LONG)
         }
 
-        val textViewCorreoEst: TextView = binding.textViewEstEmail
-        profileDriveViewModel.emailDriver.observe(viewLifecycleOwner) {
-            textViewCorreoEst.text = it
-        }
-
-        val textViewCelularEst: TextView = binding.textViewEstTelefono
-        profileDriveViewModel.numberDriver.observe(viewLifecycleOwner) {
-            textViewCelularEst.text = it
-        }
         return root
     }
 
