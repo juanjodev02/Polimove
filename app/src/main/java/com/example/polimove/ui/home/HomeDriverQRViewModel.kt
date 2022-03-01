68339a1182a9103188529649acaf2f88fdbe7ddb
Andrea Cachipuendo <andreavs05@hotmail.com>
2022-02-28 20:09:52 -0500

    Modified HomeDriverQRViewModel.kt and fragment_home_driverqr.xml

--- app/src/main/java/com/example/polimove/ui/home/HomeDriverQRViewModel.kt
+++ app/src/main/java/com/example/polimove/ui/home/HomeDriverQRViewModel.kt
@@ -1,4 +1,22 @@
 package com.example.polimove.ui.home
 
-class HomeDriverQRViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeDriverQRViewModel: ViewModel(){
+
+        private val _QRCODE = MutableLiveData<String>().apply {
+            value = "QRCODE"
+        }
+        val QRCODEDRIVER: LiveData<String> = _QRCODE
+        private val _nameStd = MutableLiveData<String>().apply {
+            value = "LESLY TIPANLUIZA"
+        }
+        val nameStd: LiveData<String> = _nameStd
+        private val _Asiento = MutableLiveData<String>().apply {
+            value = "12-B"
+        }
+        val numberAsiento: LiveData<String> = _Asiento
+
 }
