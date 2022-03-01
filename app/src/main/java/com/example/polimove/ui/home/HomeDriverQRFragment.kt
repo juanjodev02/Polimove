75e3b9e0f6bcae67e8e8cdf030e806d5a5728a48
Andrea Cachipuendo <andreavs05@hotmail.com>
2022-02-28 20:18:17 -0500

    Modified HomeDriverQRViewModel.kt and fragment_home_driverqr.xml modifieding errors.

--- app/src/main/java/com/example/polimove/ui/home/HomeDriverQRFragment.kt
+++ app/src/main/java/com/example/polimove/ui/home/HomeDriverQRFragment.kt
@@ -1,4 +1,49 @@
 package com.example.polimove.ui.home
 
-class HomeDriverQRFragment {
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.polimove.databinding.FragmentHomeBinding
+
+class HomeDriverQRFragment: Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val homeViewModel =
+            ViewModelProvider(this).get(HomeViewModel::class.java)
+
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val root: View = binding.root
+
+        val QR: TextView = binding.textViewQRCODE
+        homeViewModel.QRCODEDRIVER.observe(viewLifecycleOwner) {
+            QR.text = it
+        }
+        val numberAsient:TextView = binding.textViewNumberAsiento
+        homeViewModel.numberAsiento.observe(viewLifecycleOwner){
+            numberAsient.text = it
+        }
+        val nameStudent:TextView=binding.textViewNameStd
+        homeViewModel.nameStd.observe(viewLifecycleOwner){
+            nameStudent.text = it
+        }
+        return root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
